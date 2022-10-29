1bbb2de02b2b847d2cfb510d37ce21c3ea6e83e1
Vivek <verma17@uwindsor.ca>
2022-10-28 23:00:59 -0400

    SL-13 establishing db usage for user_details end points

--- SlsRunner.py
+++ SlsRunner.py
@@ -1,3 +1,4 @@
+import sqlalchemy as db
 from flask import Flask
 from flask import request
 
@@ -23,6 +24,14 @@ class QnA:
 
 
 app = Flask(__name__)
+print("Firing up db connection from the server")
+engine = db.create_engine("cockroachdb://root:@localhost:32412/defaultdb")
+conn = engine.connect()
+print("DB connected!")
+md = db.MetaData()
+user_detail_ds = db.Table('user_detail', md, autoload=True, autoload_with=engine)
+user_qna_ds = db.Table('user_qna', md, autoload=True, autoload_with=engine)
+qna_ds = db.Table('qna', md, autoload=True, autoload_with=engine)
 
 
 @app.route('/')
@@ -34,6 +43,16 @@ def base_location():
 def create_user_detail():
     print(request.json)
     user = UserDetail(**request.json)
+
+    print("Prepping sql stmt")
+    row = user_detail_ds.insert().values(
+        name=user.name,
+        email=user.email,
+        password=user.password
+    )
+    print(f"Executing sql: {row}")
+    conn.execute(row)
+
     return f"Insertion of data of {user.name} done"
 
 
@@ -41,17 +60,44 @@ def create_user_detail():
 def update_user_detail():
     print(request.json)
     user = UserDetail(**request.json)
+
+    print("Prepping sql stmt")
+    row = user_detail_ds.insert().values(
+        name=user.name,
+        email=user.email,
+        password=user.password
+    )
+    print(f"Executing sql: {row}")
+    conn.execute(row)
+
     return f"Update of data of {user.name} done"
 
 
 @app.route('/user/detail/<user_id>', methods=['GET'])
 def get_user_detail(user_id):
-    return "Done"
+    row = user_detail_ds.select().where(user_detail_ds.c.email == user_id)
+    result = conn.execute(row)
+    user = None
+    for res in result:
+        user = UserDetail(res[0], res[1], name=f"{res[2]}")
+        break
+
+    print(user.__dict__)
+    return user.__dict__
 
 
 @app.route('/user/detail/verify', methods=['POST'])
 def verify_user_detail():
     user = UserDetail(**request.json)
+    row = user_detail_ds.select() \
+        .where(user_detail_ds.c.email == user.email) \
+        .where(user_detail_ds.c.password == user.password)
+    result = conn.execute(row)
+    found = False
+    for res in result:
+        print(res)
+        found = True
+        break
     return f"User '{user.email}' exist status: {found}"
 
 
